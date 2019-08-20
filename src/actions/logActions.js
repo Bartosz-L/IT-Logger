bd3db413ce035a1c6f391565ce33ea16c5c999b6
Bartosz Ligęza <bligeza@tidk.pl>
2019-08-20 13:04:21 +0200

    delete Logs from server using redux

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,10 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG
+} from './types';
 
 // get logs from server
 export const getLogs = () => {
@@ -12,6 +18,29 @@ export const getLogs = () => {
       dispatch({
         type: GET_LOGS,
         payload: data
+      });
+    } catch (error) {
+      dispatch({
+        type: LOGS_ERROR,
+        payload: error.response.data
+      });
+    }
+  };
+};
+
+// delete log from server
+export const deleteLog = id => {
+  return async (dispatch, getState) => {
+    try {
+      setLoading();
+
+      await fetch(`/logs/${id}`, {
+        method: 'DELETE'
+      });
+
+      dispatch({
+        type: DELETE_LOG,
+        payload: id
       });
     } catch (error) {
       dispatch({
