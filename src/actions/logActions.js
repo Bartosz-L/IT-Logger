ab3c963cc113d689553a1d88d4b2ff1a9258fe2d
Bartosz Ligęza <bligeza@tidk.pl>
2019-08-20 15:12:52 +0200

    search logs using redux

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -5,6 +5,7 @@ import {
   ADD_LOG,
   UPDATE_LOG,
   DELETE_LOG,
+  SEARCH_LOGS,
   SET_CURRENT,
   CLEAR_CURRENT
 } from './types';
@@ -72,6 +73,28 @@ export const updateLog = log => {
 
       dispatch({
         type: UPDATE_LOG,
+        payload: data
+      });
+    } catch (error) {
+      dispatch({
+        type: LOGS_ERROR,
+        payload: error.response.data
+      });
+    }
+  };
+};
+
+// search logs
+export const searchLogs = text => {
+  return async (dispatch, getState) => {
+    try {
+      setLoading();
+
+      const res = await fetch(`/logs?q=${text}`);
+      const data = await res.json();
+
+      dispatch({
+        type: SEARCH_LOGS,
         payload: data
       });
     } catch (error) {
--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { searchLogs } from '../../actions/logActions';
 
-const SearchBar = () => {
+const SearchBar = ({ searchLogs }) => {
+  const text = useRef('');
+
+  const handleOnChange = e => {
+    searchLogs(text.current.value);
+  };
+
   return (
     <nav style={{ marginBottom: '30px' }} className="blue">
       <div className="nav-wrapper">
         <form>
           <div className="input-field">
-            <input id="search" type="search" />>
+            <input
+              id="search"
+              type="search"
+              placeholder="Search Logs..."
+              autoComplete="off"
+              ref={text}
+              onChange={handleOnChange}
+            />
             <label className="label-icon" htmlFor="search">
               <i className="material-icons">search</i>
             </label>
@@ -18,4 +34,11 @@ const SearchBar = () => {
   );
 };
 
-export default SearchBar;
+SearchBar.propTypes = {
+  searchLogs: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { searchLogs }
+)(SearchBar);
